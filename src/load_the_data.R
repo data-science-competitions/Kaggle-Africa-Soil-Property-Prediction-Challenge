74b9b98c9d8b4b0358934caef69590f00b30c8b5
harell <harel@loostiger.com>
2017-07-03 19:35:11 +0300

    split the dataset into two variables groups

--- src/load_the_data.R
+++ src/load_the_data.R
@@ -6,6 +6,19 @@ test <- read.csv(file.path(getwd(),"data","test.csv.gz"))
 sampleSubmission <- read.csv(file.path(getwd(),"data","sampleSubmission.csv.gz"))    
 
 
+#####################################
+# Split the variables into 2 groups #
+#####################################
+#' * Full spectrum = [2:3579]
+#' * Relevant spectrum = [1902:2579]
+train.infrared <- train[,2:3579]
+train.spatial <- train[,3580:3595]
 
+test.infrared <- test[,2:3579]
+test.spatial <- test[,3580:3595]
 
+Y_labels = c("Ca","P","pH","SOC","Sand")
+train.Y <- train[,Y_labels]
+
+rm("train","test")
 
